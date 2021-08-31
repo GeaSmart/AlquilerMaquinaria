323ae1d5d2d7c72ae8faf9e00cf3d42565518a20
Gerson Azabache <gerson_dunk@hotmail.com>
2021-08-30 23:06:39 -0500

    maquinaria classes fixed

--- Model/DTO/MaquinariaDTO.cs
+++ Model/DTO/MaquinariaDTO.cs
@@ -10,8 +10,13 @@ namespace Model.DTO
     {
         public int id { get; set; }
         public string equipo { get; set; }
+        public string numero_serie { get; set; }
         public string marca { get; set; }
         public string modelo { get; set; }
         public string estado { get; set; }
+        public DateTime fecha_compra { get; set; }
+        public int ciclo_horas_mtto { get; set; }
+        public int horas_alquiler_defecto { get; set; }
+        public decimal precio_dia_defecto { get; set; }
     }
 }
